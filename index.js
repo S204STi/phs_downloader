9385ad2f4793bd0311f04318ddcc203563485535
Roy <rechase84@gmail.com>
2016-08-28 17:14:30 -0600

    initial commit

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 'use strict';
 
-const {app, BrowserWindow} = require('electron');
+const {
+    app,
+    BrowserWindow
+} = require('electron');
 
 //global reference of the window object to prevent window close on garbage collection
 let win;
 
-function createWindow () {
-	win = new BrowserWindow({width: 800, height: 600});
+function createWindow() {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600
+    });
 
-	//html file to be opened
-	win.loadURL('file://${__dirname}/index.html');
+    //html file to be opened
+    win.loadURL('file://${__dirname}/index.html');
 
-	//cleanup on window close
-	win.on('closed', () => {
-		win = null;
-	});
+    //cleanup on window close
+    win.on('closed', () => {
+        win = null;
+    });
 }
 
 //create window at startup
@@ -22,13 +28,13 @@ app.on('ready', createWindow);
 
 //quit on windows close
 app.on('window-all-closed', () => {
-	if (process.platform !== 'darwin') {
-		app.quit();
-	}
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
 });
 
 app.on('activate', () => {
-	if(win === null){
-		createWindow();
-	}
+    if (win === null) {
+        createWindow();
+    }
 });
