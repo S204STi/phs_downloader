03e8d7cf3cd4ec359c54747d4308d608cf1dee87
Roy <rechase84@gmail.com>
2016-08-28 19:07:46 -0600

    nevermind that wasn't the problem. can now show angular assets

--- app/controllers.js
+++ app/controllers.js
@@ -1,1 +1,15 @@
 'use strict';
+
+app.controller('appController', ['screen', function(screen){
+	screen.show('./index.html');
+}]);
+
+app.controller('downloadController', ['downloadService', function(downloadService){
+	var vm = this;
+	vm.downloadAll = downloadService.downloadAll();
+}]);
+
+app.controller('headerController', [function(){
+	var vm = this;
+	console.log(vm);
+}]);
--- app/directives.js
+++ app/directives.js
@@ -1,1 +1,8 @@
 'use strict';
+
+app.directive('ngHeader', function(){
+	return {
+		templateUrl: './partials/_header.html',
+		controller: "headerController as HDC"
+	};
+});
